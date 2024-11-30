52e870a94d89b1ebcf3f4490597f13b84bc6e45f
Giorgi <killervision321@gmail.com>
2024-11-30 16:43:03 +0400

    Implement cart screen with item list and checkout button

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -1,13 +1,33 @@
-import { StatusBar } from "expo-status-bar"
-import { Platform, Text, View } from "react-native"
+import { StatusBar } from 'expo-status-bar'
+import { FlatList, Platform, View, Text } from 'react-native'
+import { useCart } from '../providers/CartProvider'
+import CartListItem from '../components/CartListItem'
+import Button from '../components/Button'
 
 const CartScreen = () => {
-	return <View>
-		<Text>Cart</Text>
+  const { items, totalValue } = useCart()
 
-		<StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
+  return (
+    <View style={{ padding: 10 }}>
+      <FlatList
+        data={items}
+        contentContainerStyle={{ gap: 10 }}
+        renderItem={({ item }) => <CartListItem cartItem={item} />}
+        keyExtractor={(item) => item.id}
+      />
 
-	</View>
+      {items.length > 0 && (
+        <View style={{ marginTop: 10 }}>
+          <Button
+            customTextStyle={{ fontSize: 20 }}
+            text={`Checkout $${totalValue.toFixed(2)}`}
+          />
+        </View>
+      )}
+
+      <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
+    </View>
+  )
 }
 
 export default CartScreen
